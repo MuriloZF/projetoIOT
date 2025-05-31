35660c9646c3a892245c819d4c491b8cb595c5e6
MuriloZF <mzimermanfortaleza@gmail.com>
2025-05-31 19:48:28 -0300

    fixed imports - added flask.flash

--- controllers/user.py
+++ controllers/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from models.user.user import User
 from models.db import db
 from functools import wraps
